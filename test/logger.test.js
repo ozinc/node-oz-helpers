067e3e7dacfe73803ea60a451cfb556a3b239102
Hrafn Eiríksson <hrafne@gmail.com>
2014-11-03 14:59:36 +0000

    Forgot .fatal and added version to each log message.

--- test/logger.test.js
+++ test/logger.test.js
@@ -14,10 +14,11 @@ describe('logger', function () {
     log.initialize('playlist_z', '0.1.0');
 
     log.trace('never gonna give you up,');
-    log.debug({ smiley: ':D' }, 'never gonna let you down');
-    log.info('never gonna run around,');
-    log.warn({ no: 42 }, 'and');
-    log.error({ err: new Error('hehe') }, 'desert you.');
+    log.debug({ smiley: ':D' }, 'never gonna let you down,');
+    log.info('never gonna run around and desert you.');
+    log.warn({ no: 42 }, 'never gonna make you cry,');
+    log.error({ err: new Error('err') }, 'never gonna say goodbye,');
+    log.fatal({ err: new Error('') }, 'never gonna tell a lie and hurt you.');
   });
   it('should not initialize without name/version', function () {
     (function () {
