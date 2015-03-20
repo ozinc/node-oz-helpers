fb059a0b1a23bc4c620f35b5ad3de993f3ef2be0
Hrafn Eiríksson <hrafne@gmail.com>
2015-03-20 19:28:20 -0400

    Added a log.requestLogger() middleware.

--- test.js
+++ test.js
@@ -1,9 +1,12 @@
 var statsd = require('./lib/statsd').getInstance();
+var log = require('./lib/logger').getInstance();
 var express = require('express');
 var RNG = require('rng');
 
 var app = express();
 var rng = new RNG('WhatAWhat');
+var requestLogger = require('morgan');
+app.use(log.requestLogger());
 
 app.get('/', statsd.middleware('user_z.requests'), function (req, res) {
   setTimeout(function () {
