0a7631b9d57950c96fc95566834a1c466c42df9a
Gunnar Cortes <gnarr.c@gmail.com>
2019-09-09 15:38:19 +0000

    changed getters from const to functions.

--- index.d.ts
+++ index.d.ts
@@ -3,7 +3,7 @@ import logger from './lib/logger';
 import statsd from './lib/statsd';
 
 declare module 'node-oz-helpers' {
-  export const getConf: typeof conf;
-  export const getLogger: typeof logger;
-  export const getStatsD: typeof statsd;
+  export function getConf(): typeof conf;
+  export function getLogger(): typeof logger;
+  export function getStatsD(): typeof statsd;
 }
